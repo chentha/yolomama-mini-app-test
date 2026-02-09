5a0e673824c129515f55edb254db0f21c78812fa
chenthahour <hourchentha00@gmail.com>
2026-02-09 17:23:07 +0700

    yolomama mini app

---
+++ src/app/core/services/cart.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { CartItem } from '../models/cart-item.model';
+import { Product } from '../models/product.model';
+
+@Injectable({ providedIn: 'root' })
+export class CartService {
+  private cart$ = new BehaviorSubject<CartItem[]>([]);
+  dataCart:any;
+
+  //get all product list
+  getCart() {
+    return this.cart$.asObservable();
+  }
+
+
+  //check product has or not
+  hasItems() {
+    console.log('has item', this.cart$.value.length > 0)
+    return this.cart$.value.length > 0;
+  }
+
+
+  //add cart product
+  add(product: any) {
+    const cart = this.cart$.value;
+    const item = cart.find(i => i.product.id === product.id);
+
+    if (item) item.qty++;
+    else cart.push({ product: product, qty: 1 });
+
+    this.cart$.next([...cart]);
+
+
+    console.log('data add cart', this.cart$)
+  }
+
+
+  //update qty product
+  updateQty(productId: number, qty: number) {
+    const cart = this.cart$.value.map(i =>
+      i.product.id === productId ? { ...i, qty } : i
+    ).filter(i => i.qty > 0);
+
+    this.cart$.next(cart);
+  }
+
+
+  //total price product
+  // total(): number {
+  //   return this.cart$.value.reduce((total, item) => {
+  //     return total + (item.price * item.qty);
+  //   }, 0);
+  // }
+
+  //count product
+  count() {
+    return this.cart$.value.reduce((sum, i) => sum + i.qty, 0);
+  }
+
+
+}
---
+++ src/app/pages/checkout/checkout-page/checkout-page.ts
@@ -0,0 +1,124 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { CartService } from '../../../core/services/cart.service';
+import { PaymentMethod } from '../../../core/services/payment-method';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-checkout-page',
+  imports: [CommonModule],
+  templateUrl: './checkout-page.html',
+  styleUrl: './checkout-page.scss',
+})
+export class CheckoutPage {
+  CartData: any[] = [];
+  TotalPrice: any;
+  selectedMethod: any;
+
+   payment_method:any;
+
+  constructor(
+    public cartService: CartService,
+    private paymentMethod: PaymentMethod
+  ){
+    
+  }
+
+  ngOnInit(){
+    this.getPaymentMethod()
+    this.getData();
+  }
+
+  getData(){
+    this.cartService.getCart().subscribe(
+      (respone:any) =>{
+        console.log('data cart', respone);
+        this.CartData = respone[0]?.product; 
+
+        if(this.CartData){
+          this.totalPrice();
+        }
+      }
+    )
+  }
+
+
+  totalPrice(): number {
+    return this.CartData?.reduce((total, item) => {
+      return total + (item.price * item.qty);
+    }, 0);
+  }
+
+
+    //increase cart
+  increase(p: any) {
+    p.qty++;
+    this.UpdatedAllData(p);
+  }
+
+
+  //decrease cart
+  decrease(p: any) {
+    if (p.qty > 0) p.qty--;
+     this.UpdatedAllData(p);
+  }
+
+  //on input in or de cart
+  onQtyChange(event: Event, p: any) {
+    const value = Number((event.target as HTMLInputElement).value);
+    console.log('qty number', p)
+
+    if (isNaN(value) || value < 0) {
+      p.qty = 0;
+    } else {
+      p.qty = value;
+    }
+  }
+
+
+    //update data added
+  UpdatedAllData(data: any) {
+    const index = this.CartData.findIndex(item => item.id === data.id);
+
+    if (data.qty > 0) {
+      if (index === -1) {
+        this.CartData.push({ ...data });
+      } else {
+        this.CartData[index].qty = data.qty; 
+      }
+    } else {
+      if (index !== -1) {
+        this.CartData.splice(index, 1); 
+      }
+    }
+
+    console.log('CartData:', this.CartData);
+  }
+
+
+  //update qty product
+  updateCart(data:any){
+    this.cartService.updateQty(data.id, data.qty);
+  }
+
+
+
+
+  getPaymentMethod(){
+    this.payment_method = this.paymentMethod.getMethods();
+    console.log('payment method', this.payment_method)
+  }
+
+
+  selectPaymentMethod(method: any) {
+    this.selectedMethod = method;
+  }
+
+  // get totalItems() {
+  //   return this.products.reduce((a, b) => a + b.qty, 0);
+  // }
+
+  // get totalPrice() {
+  //   return this.products.reduce((a, b) => a + (b.qty * b.price), 0);
+  // }
+}
