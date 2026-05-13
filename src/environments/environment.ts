7200a86772d2cb1f71396b36095a69d48996e9e1
chenthahour <hourchentha00@gmail.com>
2026-05-13 13:44:30 +0700

    latest 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   // baseAPI: 'http://44.250.104.186:8002',
-  // baseAPI: 'https://epass-api.gbstech.com.kh',
-  baseAPI: "https://epass-yolomama.loca.lt",
+  baseAPI: 'https://epass-api.gbstech.com.kh',
+  // baseAPI: "https://epass-yolomama.loca.lt",
   baseToken : '',
   localEncriptKey: 'GBS@2015',
 };
